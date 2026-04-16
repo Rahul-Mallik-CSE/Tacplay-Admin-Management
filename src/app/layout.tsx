ab8ad8185558e5b5fb89400f5e11101f8ddfb64f
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-16 16:39:22 +0600

    feat: wrap LayoutWrapper with ReduxProvider in RootLayout and add proxy middleware for session management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/CommonComponents/LayoutWrapper";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LayoutWrapper>{children}</LayoutWrapper>
+        <ReduxProvider>
+          <LayoutWrapper>{children}</LayoutWrapper>
+        </ReduxProvider>
       </body>
     </html>
   );
