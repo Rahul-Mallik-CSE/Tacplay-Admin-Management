23738ef42768ab3a315656a1dd4d9611aa3bdcdc
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-16 16:37:08 +0600

    feat: implement authentication and session management with Redux

---
+++ src/redux/store.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+import baseAPI from "@/redux/api/baseAPI";
+import authReducer from "@/redux/features/auth/authSlice";
+export const store = configureStore({
+  reducer: {
+    [baseAPI.reducerPath]: baseAPI.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseAPI.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
